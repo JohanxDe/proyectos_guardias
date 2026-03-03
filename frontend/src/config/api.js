a970671b3e873e1cbf5eec2fadb431e0000a177d
johan <johan020497@gmail.com>
2026-03-03 00:22:13 -0300

    se agrega api a api.js

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -2,11 +2,11 @@ export const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:50
 
 export const API_ENDPOINTS = {
     AUTH: {
-        LOGIN: `${API_BASE_URL}/auth/login`,
-        REGISTER: `${API_BASE_URL}/auth/register`,
-        PERFIL: `${API_BASE_URL}/auth/perfil`,
-        REGISTRO_ADMIN: `${API_BASE_URL}/auth/registro-admin`,
+        LOGIN: `${API_BASE_URL}/api/auth/login`,
+        REGISTER: `${API_BASE_URL}/api/auth/register`,
+        PERFIL: `${API_BASE_URL}/api/auth/perfil`,
+        REGISTRO_ADMIN: `${API_BASE_URL}/api/auth/registro-admin`,
     },
-    TRABAJOS: `${API_BASE_URL}/trabajos`,
-    NOTICIAS: `${API_BASE_URL}/noticias`,
+    TRABAJOS: `${API_BASE_URL}/api/trabajos`,
+    NOTICIAS: `${API_BASE_URL}/api/noticias`,
 }
